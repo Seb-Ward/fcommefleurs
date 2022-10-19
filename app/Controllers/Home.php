8094793374c124b4b816df2abbd9352c87751b1c
Romain Maingre <contact@romainmaingre.com>
2022-10-19 18:37:08 +0200

    Update product with categorie information

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -13,8 +13,8 @@ class Home extends BaseController
 
         $this->data['page'] = "home";
         $this->data['content'] = view('home',array(
-            'trendy_collection'=> transformItemsToObjects($productModel->getProduct(null,array("trendy_collection"=>true,"home_page"=>true))),
-            'monthly_offer'=> transformItemsToObjects($productModel->getProduct(null,array("monthly_offer"=>true,"home_page"=>true)))
+            'trendy_collection'=> transformItemsToObjects($productModel->getProduct(null,array("categorie_id"=>1,"home_page"=>true))),
+            'monthly_offer'=> transformItemsToObjects($productModel->getProduct(null,array("categorie_id"=>2,"home_page"=>true)))
         ));
         return view('application', $this->data);
     }
